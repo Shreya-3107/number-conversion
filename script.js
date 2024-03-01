7d35315c2a36c87f00a24094eccf5f983f4495c3
Shreya <srishreya3107@gmail.com>
2024-03-01 23:13:37 +0530

    updated style

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ document
     var inputType = document.getElementById("input-type").value;
     var outputType = document.getElementById("convert-type").value;
     var inputNum = document.getElementById("inputNumber").value;
-    var opDisplay = document.getElementById("output");
+    var opTypeDisplay = document.getElementById("showType");
+    var opValueDisplay = document.getElementById("showValue");
     var copy_input = parseInt(inputNum);
     var opNum;
     var inputCopy = parseInt(inputNum);
     var digit;
-    var i;
     var pow;
 
     var nod = copy_input.toString().length;
@@ -21,53 +21,69 @@ document
         //binary to other types
 
         switch (outputType) {
-            case "bin":
-                opNum = inputNum;
-                opDisplay.innerHTML = opNum;
-                break;
+          case "bin":
+            opNum = inputNum;
+            opDisplay.innerHTML = opNum;
+            break;
 
-            case "oct":
-                
-                var num;
-                pow = 1;
-                opNum = 0;
+          case "oct":
+            var num;
+            pow = 1;
+            opNum = 0;
 
-                while( inputCopy>0 ){
-                    digit = inputCopy % 1000;
-                    switch(digit){
-                        case 0: num = 0; break;
-                        case 1: num = 1; break;
-                        case 10: num = 2; break;
-                        case 11: num = 3; break;
-                        case 100: num = 4; break;
-                        case 101: num = 5; break;
-                        case 110: num = 6; break;
-                        case 111: num = 7; break;
-                    }
-                    opNum = opNum + ( num*pow );
-                    inputCopy = Math.floor(inputCopy / 1000);
-                    pow = pow * 10;
-                }
+            while (inputCopy > 0) {
+              digit = inputCopy % 1000;
+              switch (digit) {
+                case 0:
+                  num = 0;
+                  break;
+                case 1:
+                  num = 1;
+                  break;
+                case 10:
+                  num = 2;
+                  break;
+                case 11:
+                  num = 3;
+                  break;
+                case 100:
+                  num = 4;
+                  break;
+                case 101:
+                  num = 5;
+                  break;
+                case 110:
+                  num = 6;
+                  break;
+                case 111:
+                  num = 7;
+                  break;
+              }
+              opNum = opNum + num * pow;
+              inputCopy = Math.floor(inputCopy / 1000);
+              pow = pow * 10;
+            }
 
-                // Display the octal equivalent
-                opDisplay.innerHTML = opNum;
-                break;
+            // Display the octal equivalent
+            opTypeDisplay.innerHTML = "octal";
+            opValueDisplay.innerHTML = opNum;
+            break;
 
-            case "dec":
-                
-                opNum = 0;
-                pow = 1;
+          case "dec":
+            opNum = 0;
+            pow = 1;
 
-                while( inputCopy>0 ){
-                    digit = inputCopy % 10;
-                    opNum = opNum + ( digit * pow );
-                    pow = pow * 2;
-                    inputCopy = Math.floor(inputCopy / 10);
-                }
+            while (inputCopy > 0) {
+              digit = inputCopy % 10;
+              opNum = opNum + digit * pow;
+              pow = pow * 2;
+              inputCopy = Math.floor(inputCopy / 10);
+            }
 
-                //Display the decimal equivalent
-                opDisplay.innerHTML = opNum;
-                break;
+            //Display the decimal equivalent
+            opTypeDisplay.innerHTML = "decimal";
+            opValueDisplay.innerHTML = opNum;
+            break;
         }
         break;
 
@@ -78,6 +94,5 @@ document
       case "dec":
         //code
         break;
-        
     }
   });
